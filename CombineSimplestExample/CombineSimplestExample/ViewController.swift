d68398eb98c1cf0c75cf99da3acfa1e4f6c5a2d1
PavelGnatyuk <pgnatyuk@gmail.com>
2020-03-29 20:30:55 +0300

    Initial Xcode project created

---
+++ CombineSimplestExample/CombineSimplestExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CombineSimplestExample
+//
+//  Created by Pavel Gnatyuk on 29/03/2020.
+//  Copyright © 2020 Pavel Gnatyuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+
+}
+
